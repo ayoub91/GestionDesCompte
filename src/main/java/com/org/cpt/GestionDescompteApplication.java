3566e6a76baeac5232438303cc3f73ba07bfbd5f
ayoub <ayoub.boudad91@gmail.com>
2019-01-27 17:45:25 +0000

    first projet

---
+++ src/main/java/com/org/cpt/GestionDescompteApplication.java
@@ -0,0 +1,14 @@
+package com.org.cpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDescompteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDescompteApplication.class, args);
+	}
+
+}
+
